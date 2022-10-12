8c0a9d29d8d6a2a1d597d2ac626d6b0afc7a1901
liuhuiliang <825378291@qq.com>
2022-10-12 22:42:43 +0800

    增加智能刷新绑定

--- app/src/main/java/com/lhl/databing/MainActivity.java
+++ app/src/main/java/com/lhl/databing/MainActivity.java
@@ -1,14 +1,78 @@
 package com.lhl.databing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.ObservableInt;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.lhl.databinding.BindData;
+import com.lhl.databinding.ui.BaseActivity;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
+
+public class MainActivity extends BaseActivity implements BindData.OnClickListener, OnLoadMoreListener, OnRefreshListener {
+    public ObservableInt type = new ObservableInt(-1);
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void bindModel() {
+        super.bindModel();
+        bindModel(BR.activity,this);
+    }
+
+    @Override
+    public int layout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void onClick(int id) {
+        type.set(-1);
+        if (id == 1) {
+            type.set(BindData.AUTO_REFRESH);
+            return;
+        }
+        if (id == 2) {
+            type.set(BindData.AUTO_LOAD_MORE);
+            return;
+        }
+        if (id == 3) {
+            type.set(BindData.FINISH_REFRESH);
+            return;
+        }
+        if (id == 4) {
+            type.set(BindData.FINISH_LOAD_MORE);
+            return;
+        }
+    }
+
+    @Override
+    public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+        Log.e("====","onLoadMore");
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                type.set(-1);
+                type.set(BindData.FINISH_LOAD_MORE);
+            }
+        }, 3000);
+    }
+
+    @Override
+    public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+        Log.e("====","onRefresh");
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                type.set(-1);
+                type.set(BindData.FINISH_REFRESH);
+            }
+        }, 3000);
     }
 }
--- databinding/src/main/java/com/lhl/databinding/BindData.java
+++ databinding/src/main/java/com/lhl/databinding/BindData.java
@@ -1,17 +1,71 @@
 package com.lhl.databinding;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.databinding.BindingAdapter;
 
+import com.scwang.smart.refresh.layout.SmartRefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshLoadMoreListener;
+
 import java.lang.ref.WeakReference;
 
 public class BindData {
+    //https://github.com/scwang90/SmartRefreshLayout/blob/master/art/md_property.md
+    public static final int AUTO_REFRESH = 1;
+    public static final int AUTO_LOAD_MORE = AUTO_REFRESH << 1;
+    public static final int FINISH_REFRESH = AUTO_LOAD_MORE << 1;
+    public static final int FINISH_LOAD_MORE = FINISH_REFRESH << 1;
+    public static final int AUTO_REFRESH_ANIMATION_ONLY = FINISH_LOAD_MORE << 1;
+    public static final int AUTO_LOAD_MORE_ANIMATION_ONLY = AUTO_REFRESH_ANIMATION_ONLY << 1;
+    public static final int FINISH_LOAD_MORE_WITH_NO_MORE = AUTO_LOAD_MORE_ANIMATION_ONLY << 1;
+    public static final int CLOSE_HEADER_OR_FOOTER = FINISH_LOAD_MORE_WITH_NO_MORE << 1;
     public static final long MIN_CLICK_TIME = 500;
+
     @BindingAdapter({"click", "id"})
     public static void bindClick(View view, OnClickListener listener, int id) {
         view.setOnClickListener(new MyClickListener(listener, id));
     }
+
+    @BindingAdapter({"type"})
+    public static void autoFinish(SmartRefreshLayout layout, int type) {
+        Log.e("autoFinish", "type:" + type);
+        if (type <= 0)
+            return;
+        if ((type & AUTO_REFRESH) == AUTO_REFRESH)
+            layout.autoRefresh();
+        if ((type & AUTO_LOAD_MORE) == AUTO_LOAD_MORE)
+            layout.autoLoadMore();
+        if ((type & AUTO_REFRESH_ANIMATION_ONLY) == AUTO_REFRESH_ANIMATION_ONLY)
+            layout.autoRefreshAnimationOnly();//自动刷新，只显示动画不执行刷新
+        if ((type & AUTO_LOAD_MORE_ANIMATION_ONLY) == AUTO_LOAD_MORE_ANIMATION_ONLY)
+            layout.autoLoadMoreAnimationOnly();//自动加载，只显示动画不执行加载
+        if ((type & FINISH_REFRESH) == FINISH_REFRESH)
+            layout.finishRefresh();//结束刷新
+        if ((type & FINISH_LOAD_MORE) == FINISH_LOAD_MORE)
+            layout.finishLoadMore();//结束加载
+        if ((type & FINISH_LOAD_MORE_WITH_NO_MORE) == FINISH_LOAD_MORE_WITH_NO_MORE)
+            layout.finishLoadMoreWithNoMoreData();//完成加载并标记没有更多数据 1.0.4
+        if ((type & CLOSE_HEADER_OR_FOOTER) == CLOSE_HEADER_OR_FOOTER)
+            layout.closeHeaderOrFooter();//关闭正在打开状态的 Header 或者 Footer（1.1.0）
+    }
+    @BindingAdapter("onLoadMoreListener")
+    public static void onLoadMoreListener(SmartRefreshLayout layout , OnLoadMoreListener listener){
+        layout.setOnLoadMoreListener(listener);
+    }
+
+    @BindingAdapter("onRefreshListener")
+    public static void onRefreshListener(SmartRefreshLayout layout , OnRefreshListener listener){
+        layout.setOnRefreshListener(listener);
+    }
+
+    @BindingAdapter("onRefreshLoadMoreListener")
+    public static void onRefreshLoadMoreListener(SmartRefreshLayout layout , OnRefreshLoadMoreListener listener){
+        layout.setOnRefreshLoadMoreListener(listener);
+    }
+
 
     public interface OnClickListener {
         void onClick(int id);
